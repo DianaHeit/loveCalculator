65d042458c6152399f7f5e2740ead99a0aa884ed
DianaHeit <diana.heit@gmx.de>
2019-10-13 21:52:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function loveFunction() {
 
   var love = Math.random();
+  // For some fair numbers because I want it to be around a 50:50 chance
   love = love * 10 + 50;
-
   love = Math.floor(love);
-
   console.log(love);
 
   // alert("Your Love-Score is " + love + "%.");
 
-  document.getElementById("result").innerHTML = "Your Love-Score is " + love + "%.";
+  // document.getElementById("result").innerHTML = "Your Love-Score is " + love + "%.";
+
+  if (love >= 51) {
+    document.getElementById("result").innerHTML = "Your Love-Score is " + love + "%. <br> High Chances of Survival !";
+  } else {
+    document.getElementById("result").innerHTML = "Your Love-Score is " + love + "%. <br> You've got to work at it.";
+  }
+
 }
